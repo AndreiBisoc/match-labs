62ba723e3289ff77938eecca4fb4d594b064379d
Andrei Bisoc <andrei.bisoc@TheAccessGroup.com>
2020-05-21 10:42:45 +0300

    Select technologies

--- src/components/CandidateForm.jsx
+++ src/components/CandidateForm.jsx
@@ -9,9 +9,22 @@ import { fetchTechnologies } from "../utils/request";
 const CandidateForm = ({ fields, onSubmit }) => {
   // 1. Set values and technologies
   const [values, setValues] = useState(fields);
+  const [technologies, setTechnologies] = useState(null);
 
   useEffect(() => {
     // 2. Get technologies from API
+    const onMount = async () => {
+      const res = await fetchTechnologies();
+      setTechnologies(
+        res.map((tech) => {
+          return {
+            value: tech.id,
+            label: tech.name,
+          };
+        })
+      );
+    };
+    onMount();
   }, []);
 
   const onChange = (e) => {
@@ -25,8 +38,13 @@ const CandidateForm = ({ fields, onSubmit }) => {
     // 3. copy values
     // find index of technologies input
     // loop through the selected values
+    const newValues = values.concat({
+      name: "technologies",
+      value: selected.map((tech) => tech.value),
+    });
+    setValues(newValues);
   };
-
+  
   const formHandler = (e) => {
     e.preventDefault();
     const obj = {};
@@ -42,21 +60,29 @@ const CandidateForm = ({ fields, onSubmit }) => {
   return (
     <>
       <form onSubmit={formHandler} className={styles.form}>
-        {values.map((field) => (
-          // 4. Check if field is not technologies
-          <div key={field.name} className={styles.field}>
-            <input
-              required
-              onChange={onChange}
-              value={field.value}
-              placeholder={field.placeholder || ""}
-              name={field.name}
-            ></input>
-          </div>
+        {values.map(
+          (field) =>
+            // 4. Check if field is not technologies
+            field.name !== "technologies" ? (
+              <div key={field.name} className={styles.field}>
+                <input
+                  required
+                  onChange={onChange}
+                  value={field.value}
+                  placeholder={field.placeholder || ""}
+                  name={field.name}
+                ></input>
+              </div>
+            ) : null
           // 5. Bring prebuilt select
           // Add onSelectChange and options
-        ))}
-
+        )}
+        <Select
+          onChange={(selected) => onSelectChange(selected)}
+          isMulti
+          className={styles.field}
+          options={technologies}
+        />
         <Button type={"submit"} variant={"secondary"} size={"medium"}>
           Submit
         </Button>
